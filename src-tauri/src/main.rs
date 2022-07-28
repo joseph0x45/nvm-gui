f0cfa38802fdae1a63fbbe0ca44cd00c90d31cf2
TheWisePigeon <zozozozeph@gmail.com>
2022-07-28 20:36:37 +0000

    added function for commands execution

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,18 @@
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
+use std::process::Command;
 
 fn main() {
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+
+fn executeCommand(command: &str) -> String {
+  let output = Command::new(command)
+    .output()
+    .expect("failed to execute process");
+  return String::from_utf8_lossy(&output.stdout).to_string();
 }
