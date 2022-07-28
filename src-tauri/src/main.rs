5cdff97fe5869040c9519445bfbe5660e4b0e6d7
TheWisePigeon <zozozozeph@gmail.com>
2022-07-28 21:57:35 +0000

    added system tray[unfinished]

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,18 +3,23 @@
   windows_subsystem = "windows"
 )]
 use std::process::Command;
+use tauri::SystemTray;
 
 #[tauri::command]
 fn execute_command(command: &str) -> String {
   let output = Command::new(command)
-    .arg("list")
+    .arg("use")
+    .arg("16.16.0")
     .output()
     .expect("failed to execute process");
   return String::from_utf8_lossy(&output.stdout).to_string();
 }
 
 fn main() {
+  let tray = SystemTray::new();
+
   tauri::Builder::default()
+    .system_tray(tray)
     .invoke_handler(tauri::generate_handler![execute_command])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
