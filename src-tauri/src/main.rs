e429301aded1d980a0f3559f48ab9174944c170b
TheWisePigeon <zozozozeph@gmail.com>
2022-07-30 23:43:36 +0000

    added main functions in utils

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,10 +6,9 @@ use std::process::Command;
 use tauri::SystemTray;
 
 #[tauri::command]
-fn execute_command(command: &str) -> String {
-  let output = Command::new(command)
+fn get_all_versions() -> String {
+  let output = Command::new("nvm")
     .arg("list")
-    .arg("")
     .output()
     .expect("failed to execute process");
   return String::from_utf8_lossy(&output.stdout).to_string();
@@ -25,10 +24,10 @@ fn get_node_version() -> String {
 }
 
 #[tauri::command]
-fn change_version()-> String {
+fn change_version(version: &str)-> String {
   let output = Command::new("nvm")
     .arg("use")
-    .arg("16.16.0")
+    .arg(version)
     .output()
     .expect("failed to execute process");
   return String::from_utf8_lossy(&output.stdout).to_string();
@@ -38,20 +37,8 @@ fn main() {
   let tray = SystemTray::new();
 
   tauri::Builder::default()
-    .setup(|app| {
-      match app.get_cli_matches() {
-        // `matches` here is a Struct with { args, subcommand }.
-        // `args` is `HashMap<String, ArgData>` where `ArgData` is a struct with { value, occurances }.
-        // `subcommand` is `Option<Box<SubcommandMatches>>` where `SubcommandMatches` is a struct with { name, matches }.
-        Ok(matches) => {
-          println!("{:?}", matches)
-        }
-        Err(_) => {}
-      }
-      Ok(())
-    })
     .system_tray(tray)
-    .invoke_handler(tauri::generate_handler![execute_command, get_node_version, change_version])
+    .invoke_handler(tauri::generate_handler![get_all_versions, get_node_version, change_version])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/utils/manager.ts
+++ src/utils/manager.ts
@@ -1,4 +1,24 @@
+import { invoke } from "@tauri-apps/api";
 
-export async function get_current_version(){
-
+export async function get_current_version() : Promise<string> {
+    let current = await invoke('get_node_version') as string
+    return current
 }
+
+export async function get_versions_list(): Promise<string> {
+    let versions = await invoke('get_all_versions') as string
+    return versions
+}
+
+export async function change_version( version: string ) : Promise<string> {
+    let change_result = await invoke('change_version', {version: version}) as string
+    return change_result
+}
+
+const manager = {
+    get_versions_list,
+    get_current_version,
+    change_version
+}
+
+export default manager
