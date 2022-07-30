099149054b205434770c291e9ff752e8ab1fdfa6
TheWisePigeon <zozozozeph@gmail.com>
2022-07-30 23:58:18 +0000

    added stuff

--- src/components/version_tile.tsx
+++ src/components/version_tile.tsx
@@ -4,16 +4,21 @@ import manager from "../utils/manager"
 export default function VersionTile(props: version){
     const { is_current, version } = props
     const [is_selected, setIsSelected] = useState(false)
+    let duh = false
     async function onClick(){
         let result = await manager.change_version(version)
         console.log(result);
+        
+    }
+    function is_ckecked() {
+        alert("Version already in use")
         
     }
     return(
         <>
             <div className=" flex space-x-2 px-4">
                 <div>
-                    <input type="radio" name="version_tile" id="vst" onClick={onClick} />
+                    <input type="radio" name="version_tile" id="vst" onClick={duh? onClick: is_ckecked } />
                 </div>
                 <div>Version {version}</div>
             </div>
