19f41d4e491d6bcb6f3b884a5e1db6d167035306
TheWisePigeon <zozozozeph@gmail.com>
2022-08-06 20:08:03 +0000

    fixed formatter export

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,4 +1,4 @@
-export default function (text: string): Array<string> {
+export default function formatter(text: string): Array<string> {
     const raw_input = text.split(' ')
     const garbage = ["*", "(Currently", "using", "64-bit", "executable"]
     const versions = [] = raw_input.filter(
