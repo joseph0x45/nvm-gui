90f7474f04d9b06f1e5efa27b4aff9ac796848a9
TheWisePigeon <zozozozeph@gmail.com>
2022-08-09 17:56:39 +0000

    added elements in garbage collector

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,6 +1,6 @@
 export default function formatter(text: string): Array<string> {
     const raw_input = text.split(' ')
-    const garbage = ["*", "(Currently", "using", "64-bit", "executable"]
+    const garbage = ["*", "(Currently", "using", "64-bit", "executable", "\n", "executable)\n", ""]
     const versions = [] = raw_input.filter(
         v=> !garbage.includes(v)
     )
