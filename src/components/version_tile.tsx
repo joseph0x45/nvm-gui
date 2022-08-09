08af64c76c481260a340f6a00f9756e34d3182a8
TheWisePigeon <zozozozeph@gmail.com>
2022-08-09 20:30:08 +0000

    added v

--- src/components/version_tile.tsx
+++ src/components/version_tile.tsx
@@ -21,7 +21,7 @@ export default function VersionTile(
     return(
         <>
             <div onClick={change_version} className=" bg-red-300 mx-6 rounded-lg flex space-x-2 px-4 hover:bg-green-400">
-                <div >{props.version}  { props.is_current?"*":"" } </div>
+                <div >v{props.version}  { props.is_current?"*":"" } </div>
             </div>
         </>
     )
