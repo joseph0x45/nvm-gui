ad6d0096b42898c77a0e1aa9f98d00571f6235f1
TheWisePigeon <zozozozeph@gmail.com>
2022-08-09 21:36:25 +0000

    added reducer and slice

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
+import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider >
+      <App />
+    </Provider>
   </React.StrictMode>
 )
+
---
+++ src/slice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from './store'
+
+// Define a type for the slice state
+interface VersionState {
+  value: boolean
+}
+
+// Define the initial state using that type
+const initialState: VersionState = {
+  value: false
+}
+
+export const versionSlice = createSlice({
+  name: 'version',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    change: state=>{
+        state.value = !state.value
+    }
+  }
+})
+
+export const {  } = versionSlice.actions
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectCount = (state: RootState) => state.counter.value
+
+export default versionSlice.reducer
