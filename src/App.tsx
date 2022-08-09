aac0ddc782abf01aea3b9e5c36878ddffd615a37
TheWisePigeon <zozozozeph@gmail.com>
2022-08-09 20:28:44 +0000

    fixed is_current boolean error

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,22 @@ import { invoke } from '@tauri-apps/api/tauri'
 import VersionTile from './components/version_tile'
 import { useEffect } from 'react'
 import formatter from './utils/formatter'
-const current = '15'
-const versions = [
-  '15',
-  '14',
-  '13',
-  '12',
-  '11',
-  '10',
-  '9',
-  '8',
-  '7',
-  '6',
-  '5',
-]
-
 
 
 function App() {
   const [currentVersion, setCurrentVersion] = useState("")
   const [all_versions, setVersions] = useState([""])
-  async function change_version() {
-    let result = await invoke('change_version') as string
-    setCurrentVersion(result)
-  }
   async function getCurrentVersion() {
     let current = await invoke('get_node_version') as string
+    current = current.replace(/(\r\n|\n|\r)/gm, "")
     setCurrentVersion(current)
+    console.log(currentVersion);
 
   }
 
   useEffect(
     ()=>{
+      getCurrentVersion()
       const fetch_versions =async () => {
         const result = await invoke('get_all_versions') as string
         return result
@@ -54,9 +38,10 @@ function App() {
       <Welcome versions_count={all_versions.length.toString()} />
       <div className=' overflow-y-scroll pt-3 space-y-3 h-40 border-2'>
         {
-          versions.map(
+          all_versions.map(
             v=>{
-              return <VersionTile version={v} is_current={v === current} />
+              
+              return <VersionTile version={v} is_current={('v'+v===currentVersion)} />
             }
           )
         }
