59208cf072be8ba5d01763ce497b3f16012bbf10
TheWisePigeon <zozozozeph@gmail.com>
2022-08-09 19:56:05 +0000

    added dialog for click on already in use version

--- src/components/version_tile.tsx
+++ src/components/version_tile.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { message } from '@tauri-apps/api/dialog';
 import manager from "../utils/manager"
 
 export default function VersionTile(
@@ -8,19 +9,19 @@ export default function VersionTile(
     }
 ){
     
-    async function onClick(){
+    async function change_version(){
+        if (props.is_current){
+            message("You are already on this version")
+            return
+        }
         let result = await manager.change_version(props.version)
-        console.log('result');    
-    }
-
-    function is_ckecked() {
-        alert("Version already in use")        
+        console.log(result);    
     }
     
     return(
         <>
-            <div className=" bg-red-300 mx-6 rounded-lg flex space-x-2 px-4">
-                <div>v {props.version}  { props.is_current?"*":"" } </div>
+            <div onClick={change_version} className=" bg-red-300 mx-6 rounded-lg flex space-x-2 px-4 hover:bg-green-400">
+                <div >v {props.version}  { props.is_current?"*":"" } </div>
             </div>
         </>
     )
