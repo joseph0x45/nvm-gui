30e9f5407b0abc4f1e4c50874654d834572b6cb4
TheWisePigeon <zozozozeph@gmail.com>
2022-08-09 18:05:47 +0000

    updated formatter | made it work properly by removing the line breaks

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,8 +1,11 @@
 export default function formatter(text: string): Array<string> {
     const raw_input = text.split(' ')
     const garbage = ["*", "(Currently", "using", "64-bit", "executable", "\n", "executable)\n", ""]
-    const versions : string[] = raw_input.filter(
+    let versions : string[] = raw_input.filter(
         v=> !garbage.includes(v)
+    )
+    versions = versions.map(
+        v=> v.split('\n')[0]
     )
     return versions
 }
