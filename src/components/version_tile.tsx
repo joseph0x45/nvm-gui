6513386f71d66ed49dbf74b0185354b910307236
TheWisePigeon <zozozozeph@gmail.com>
2022-08-10 19:47:37 +0000

    added current version state in version tiles

--- src/components/version_tile.tsx
+++ src/components/version_tile.tsx
@@ -9,21 +9,21 @@ export default function VersionTile(
         "version": string
     }
 ){
-    const [is_current, setIsCurrent] = useState('')
+    const versionState = useAppSelector(state => state.version)
     const dispatch = useAppDispatch()
     async function change_version(){
-        if (props){
+        if (props.version==versionState.value){
             message("You are already on this version")
             return
         }
         let result = await manager.change_version(props.version)
-        dispatch(change('v14.17.3'))
+        dispatch(change(props.version))
     }
     
     return(
         <>
             <div onClick={change_version} className=" bg-red-300 mx-6 rounded-lg flex space-x-2 px-4 hover:bg-green-400">
-                <div >v{props.version}  { is_current?"*":"" }  </div>
+                <div >v{props.version}    </div>
             </div>
         </>
     )
