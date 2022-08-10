c4288001ee02325b236380c54ed7d1e34fef8c5a
TheWisePigeon <zozozozeph@gmail.com>
2022-08-10 20:09:09 +0000

    added manifest embedding for first release

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,6 +1,6 @@
-//extern crate embed_resource;
+extern crate embed_resource;
 
 fn main() {
-  //embed_resource::compile("nvmgui-manifest.rc");
+  embed_resource::compile("nvmgui-manifest.rc");
   tauri_build::build()
 }
